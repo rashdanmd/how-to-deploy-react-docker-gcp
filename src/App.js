dad8eaa229b7fb877ebb8a29bf588aab9b77c817
rashdanmd <rashdan.developer@gmail.com>
2023-04-27 16:08:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>How to deploy a React app with GCP</p>
         <a
           className="App-link"
           href="https://reactjs.org"
